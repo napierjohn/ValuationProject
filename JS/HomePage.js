6147a041605f707214c675b2b7ee600961ef3128
napierjohn <napierjohn@gmail.com>
2018-07-12 17:32:15 -0400

    Set tab content - need to remove <a> or<p> from tabbed link

--- JS/HomePage.js
+++ JS/HomePage.js
@@ -1,1 +1,15 @@
-﻿window.alert("help")
+﻿
+
+function openProperty(evt, propName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(propName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
